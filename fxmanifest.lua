adc28cabb1fbf2781a2229c0e0f64c258ec81278
綾小路 ろいと <ayanokoji.roito@gmail.com>
2025-03-12 08:02:07 +0900

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,4 @@ author 'Null'
 description '時給振込スクリプト'
 version '1.0.0'
 
--- サーバースクリプト
 server_script 'server.lua'
-
--- クライアントスクリプト（必要に応じて）
--- client_script 'client.lua'
-
--- 他のリソースが必要な場合はここに追加 
